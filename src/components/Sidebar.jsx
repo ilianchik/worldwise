8d236d3cf12ab4ccb118ecf2f131f90a944edc40
Ilia Kozachyshyn <kozachishin543@gmail.com>
2024-01-15 21:01:33 +0200

    first

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+      <footer className={styles.footer}>
+        &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+      </footer>
+    </div>
+  );
+}
+export default Sidebar;
